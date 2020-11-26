6907569b5efe346b112a14c02527eafad468151e
Vladislav <75069674+InventorVlad@users.noreply.github.com>
2020-11-26 17:31:58 +0600

    Add files via upload

---
+++ Ages1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	setlocale(0, "Ru");
+	string name, age, surname;
+	cout << "введите имя" << endl;
+	cin >> name;
+	cout << "введите фамилию" << endl;
+	cin >> surname;
+	cout << "введите возраст" << endl;
+	cin >> age;
+	cout <<"Ваше имя "<< name <<" "<< "Ваша фамилия "<< surname <<" "<<"Ваш возраст "<< age << endl;
+	return 0;
+
+}
