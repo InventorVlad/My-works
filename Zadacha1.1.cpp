4f6ad05c4c85fbe6facce2bdd889ab7f3ac30cff
InventorVlad <75069674+InventorVlad@users.noreply.github.com>
2020-11-26 16:03:12 +0600

    Add files via upload

---
+++ Zadacha1.1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	cout << "Подойти к  методисту" << endl;
+	cout << "Ввести номер кабинета" << endl;
+	int nomer = 0;
+	cin >> nomer;
+	cout << "Далее вы подошли к вахтеру" << endl; 
+	cout << "Методист сказал,что номер:" << nomer << "свободен,дайте ключи" << endl;
+	cout << "Вы довольные с ключами от" << nomer << "идете домой" << endl;
+	return 0;
+
+}
