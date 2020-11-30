9544b650286943353aa00b0000b721c0acfb0ebc
Vladislav <75069674+InventorVlad@users.noreply.github.com>
2020-11-30 15:12:32 +0600

    Add files via upload

---
+++ Zadacha1.3.cpp
@@ -0,0 +1,14 @@
+﻿
+#include <iostream>
+#include <windows.h>
+#include <string>
+using namespace std;
+int main()
+{
+	setlocale(0, "Ru");
+	cout << "Введите имя персонажа" << endl;
+	string name;
+	cin >> name;
+	cout << name << " вы получили квест по зачистке пещеры от гоблинов" << endl;
+ }
+
