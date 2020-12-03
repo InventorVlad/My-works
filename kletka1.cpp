30d2eff553b834fd014f39cd64abf5a0011246c4
Vladislav <75069674+InventorVlad@users.noreply.github.com>
2020-12-03 17:48:09 +0600

    Add files via upload

---
+++ kletka1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(0, "Ru");
+	int lvl, lvl1;
+	lvl = 0;
+
+	while (lvl < 15)
+	{
+		cin >> lvl1;
+		if (lvl1 < lvl + 10) {
+			if (lvl1 == lvl)
+				cout << "Идем дальше" << endl;
+			else if (lvl1 > lvl) {
+				cout << "Прыжок в верх" << endl;
+			}
+			else {
+				cout << "Прыжок вниз" << endl;
+			}
+			lvl = lvl1;
+		}
+		else if (lvl1 > lvl - 10) {
+			cout << "Слишком высоко" << endl;
+		}else{
+			cout << "Слишком низко" << endl;
+		}
+	}
+	
+}
+
