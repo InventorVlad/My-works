d189c8034e4eb1dc64f6dc92b9ba1957b2ec5bf7
Vladislav <75069674+InventorVlad@users.noreply.github.com>
2020-12-07 15:49:53 +0600

    Update Login.cpp

--- Login.cpp
+++ Login.cpp
@@ -1,16 +1,15 @@
-﻿#include <iostream>
+#include <iostream>
 #include <string>
 using namespace std;
 
 int main() {
 	setlocale(0, "Rus");
-	string login;
-	cin >> login;
-	if (login == "Vlad")
+	string login("Vlad"),oldlogin("Vlad1"),yourlogin;
+	cin >> yourlogin;
+	if (login == yourlogin)
 		cout << "Приветствуем!" << endl;
-	else if (login == "Vlad1")
+	else if (yourlogin == oldlogin)
 		cout << "Логин был изменен" << endl;
 	else
 		cout << "Такого логина не существует" << endl;
-}
-	
+}	
