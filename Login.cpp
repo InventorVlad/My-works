469df7cfb5b3007e8ba3a87500eb8be572e78704
Vladislav <75069674+InventorVlad@users.noreply.github.com>
2020-12-07 15:41:44 +0600

    Add files via upload

---
+++ Login.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	setlocale(0, "Rus");
+	string login;
+	cin >> login;
+	if (login == "Vlad")
+		cout << "Приветствуем!" << endl;
+	else if (login == "Vlad1")
+		cout << "Логин был изменен" << endl;
+	else
+		cout << "Такого логина не существует" << endl;
+}
+	
