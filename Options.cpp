0ad5d9d15cb84a5ec9ab724f15e2af0df02c5c27
Vladislav <75069674+InventorVlad@users.noreply.github.com>
2021-01-21 00:21:35 +0600

    Update Options.cpp

--- Options.cpp
+++ Options.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
 
 void Num(int& x) {
@@ -10,8 +10,12 @@ int Num1(int x) {
 	return x;
 }
 
+void Num2(int* x) {
+	*x=*x+1;
+}
+
 int main() {
-	int b,i;
+	int b, i;
 	int var = 0;
 	var = var + 1;
 	var++;
@@ -20,12 +24,16 @@ int main() {
 	Num(var);
 	b = Num1(var);
 	var = b;
+	Num2(&var);
 	for (i = 0; i != 1; i++) {
 		var = var + 1;
 	}
 	while (var < var + 1) {
 		var = var + 1;
 		break;
+	}
+	if (var > 1) {
+		var++;
 	}
 	cout << var << endl;
 
