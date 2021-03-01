e66ffff154410cb3cbd7a1be33e5ce59fb7e0f51
Vladislav <75069674+InventorVlad@users.noreply.github.com>
2021-03-01 17:58:28 +0600

    Add files via upload

---
+++ Urok01.03.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+/*int main() {
+	int a = 10;
+	int* p = &a;
+	string* d = new string("Hello world");
+	int* p_d = new int (a);
+	cout << a << endl;
+	cout << *p << endl;
+	cout << *p_d << endl;
+	a = 20;
+	cout << *p_d << endl;
+	cout << *d << endl;
+	delete d;
+	string c = ("Hello");
+	d = &c;
+	cout << *d << endl;
+}*/
+void foo(string *hello1) {
+	hello1 = NULL;
+	hello1 = new string("Hello world");
+}
+
+int main() {
+	string*a;
+	foo(a);
+		cout << *a << endl;
+}
