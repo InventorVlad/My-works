f67a35041e1cb72585228e3c4d4557943e6a7b2e
Vladislav <75069674+InventorVlad@users.noreply.github.com>
2021-03-04 17:35:09 +0600

    Add files via upload

---
+++ Urok4.03.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+
+/*int main() {
+	setlocale(0, "rus");
+	int* a=new int;
+	cin >> *a;
+	if (*a > -1 * (*a)) {
+		cout << *a << endl;
+	}
+	else {
+		cout << -1 * (*a) << endl;
+	}
+	delete a;
+	a = 0;
+
+
+
+}*/
+
+int* mod(int*a) {
+	if (*a > -1 * (*a)) {
+		return a;
+	}
+	else {
+		*a = -1 * (*a);
+		return a;
+	}
+
+}
+
+int main() {
+	int* a=new int;
+	cin >> *a;
+	mod(a);
+	cout << *a << endl;
+	delete a;
+	a = 0;
+}
