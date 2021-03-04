3f3441a137bb24b87deac1a92361ccebb601a4a2
Vladislav <75069674+InventorVlad@users.noreply.github.com>
2021-03-04 17:05:02 +0600

    Update Urok01.03.cpp

--- Urok01.03.cpp
+++ Urok01.03.cpp
@@ -1,23 +1,6 @@
 ﻿#include <iostream>
 using namespace std;
 
-/*int main() {
-	int a = 10;
-	int* p = &a;
-	string* d = new string("Hello world");
-	int* p_d = new int (a);
-	cout << a << endl;
-	cout << *p << endl;
-	cout << *p_d << endl;
-	a = 20;
-	cout << *p_d << endl;
-	cout << *d << endl;
-	delete d;
-	string c = ("Hello");
-	d = &c;
-	cout << *d << endl;
-}*/
-
 string* foo() {
 	string* hello1;
 	hello1 = new string ("Hello world");
